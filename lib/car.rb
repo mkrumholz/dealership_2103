e8df075e948a0f38c288722ea90b58abfe1d22dd
Molly Krumholz <mkkrumholz@gmail.com>
2021-03-31 14:15:22 -0600

    Complete initialize method for Car class

---
+++ lib/car.rb
@@ -0,0 +1,10 @@
+class Car
+
+  attr_reader :make, :model, :monthly_payment, :loan_length
+  def initialize(brand, monthly_payment, loan_length)
+    @make = brand.split(" ")[0]
+    @model = brand.split(" ")[1]
+    @monthly_payment = monthly_payment
+    @loan_length = loan_length
+  end
+end
