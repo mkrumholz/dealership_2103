65189aa00709b74720f19a6efa0df58af5224f7c
Molly Krumholz <mkkrumholz@gmail.com>
2021-03-31 14:40:47 -0600

    Complete functionality for Iteration 2

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,10 +1,20 @@
 class Dealership
 
   attr_reader :name,
-              :address
-              
+              :address,
+              :inventory
+
   def initialize(name, address)
     @name = name
     @address = address
+    @inventory = []
+  end
+
+  def inventory_count
+    @inventory.length
+  end
+
+  def add_car(car)
+    @inventory << car
   end
 end
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -21,5 +21,41 @@ describe Dealership do
 
       expect(dealership.address).to eq "123 Main Street"
     end
+
+    it 'starts out with no inventory' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+      expect(dealership.inventory).to eq []
+    end
+  end
+
+  describe '#inventory_count' do
+    it 'knows the count of items in inventory' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+      expect(dealership.inventory_count).to eq 0
+    end
+  end
+
+  describe '#add_car' do
+    it 'can add a car to the inventory' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+
+      expect(dealership.inventory).to eq [car_1, car_2]
+    end
+
+    it 'increases inventory_count for each car added' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+
+      expect(dealership.inventory_count).to eq 2
+    end
   end
 end
